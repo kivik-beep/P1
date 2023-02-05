b6b0b1971de2dd9cec929500bf7dbaab18298315
K.Kivi <72075784+kivik-beep@users.noreply.github.com>
2023-02-05 02:15:55 +0200

    CDFplotting added

--- P1.py
+++ P1.py
@@ -1,25 +1,36 @@
 import numpy as np
-
-A = np.random.randint(2, size = (1000000,1000))
-B = np.random.randint(2, size = (1000,1000000))
-C = np.random.randint(2, size = (1000000,1))
-
-print(f"Matrix A:\n {A}\n")
-print(f"Matrix B:\n {B}\n")
-print(f"Matrix C:\n {C}\n")
-
-D = np.matmul(A,B)
-#print(f"Matrix D:\n {D}\n")
+import matplotlib.pyplot as plt
 
 
-def multiply(A,B):
-    global result
-    if A.shape[1] == B.shape[0]:
-        result = np.zerps((A.shape[0],B.shape[1]), dtype = int)
-        for row in range(rows):
-            for col in range(cols):
-                for elt in range(len(B)):
-                    result[row, col] += A[row, elt] * B[elt, col]
-        return result
-    else:
-        return "sorry fail"
+def main():
+    A = np.random.randint(2, size = (10000,1000))
+    B = np.random.randint(2, size = (1000,100000))
+    C = np.random.randint(2, size = (100000,1))
+
+    print(f"Matrix A:\n {A}\n")
+    print(f"Matrix B:\n {B}\n")
+    print(f"Matrix C:\n {C}\n")
+
+    plot(A, "matrix A")
+    #D = np.matmul(np.matmul(A,B), C)
+    #print(f"Matrix D:\n {D}\n")
+
+
+def plot(A, name):
+    #CDF
+    x, counts = np.unique(A, return_counts=True)
+    cumsum = np.cumsum(counts)
+    y = cumsum / cumsum[-1]
+
+    x = np.insert(x, 0, x[0])
+    y = np.insert(y, 0, 0.)
+    plt.xlabel("Value")
+    plt.ylabel("%")
+    plt.title(f"CDF {name}")
+    plt.plot(x, y, drawstyle='steps-post')
+    plt.grid(True)
+    plt.show()
+
+
+if __name__=="__main__":
+    main()
